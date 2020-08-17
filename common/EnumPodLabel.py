cd19238d5e70d62005d3d105f3bc577fe323eb6a
zhaikun <zk662144@163.com>
2020-08-17 15:18:29 +0800

    EnumPodLable中新增方法get_labels_via_name

--- common/EnumPodLabel.py
+++ common/EnumPodLabel.py
@@ -10,6 +10,21 @@ class PodLabel(Enum):
     EXECUTOR = 'spark-role=executor'
 
 
+    def get_labels_via_name(label):
+        label_list = {}
+
+        for label_info in PodLabel:
+            if label_info == label:
+                labels = label_info.value.split(',')
+                for label in labels:
+                    l_info = str(label).split('=')
+
+                    label_list[l_info[0]] = l_info[1]
+                break
+
+        return label_list
+
+
 if __name__=="__main__":
     a = PodLabel.EXECUTOR.name
     b = PodLabel.EXECUTOR.value
